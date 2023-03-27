561ed68c04f40329b91640cb15c10a729a9add51
Zavalniuk-Nazar <nazarzaval99@gmail.com>
2023-03-27 18:23:58 +0300

    Created a title

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PrimaryButton from './components/button/PrimaryButton';
+import Title from './components/title/Title';
 
 function App() {
   return <div className='app'></div>;
---
+++ src/components/title/Title.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Title.css';
+
+function Title(props) {
+  return (
+    <div class='title'>
+      <h1 class='title-text'>Shoping List</h1>
+    </div>
+  );
+}
+
+export default Title;
