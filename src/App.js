f263d82374950084fc5869e11796d5cce5dea84b
Zavalniuk-Nazar <nazarzaval99@gmail.com>
2023-04-08 18:37:31 +0300

    Recreated the layout of the site from the "React" components and wrote down the basic style rules for the page in the "App.css" file.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import './style/App.css';
+import Switch from './components/switch/Switch';
+import Title from './components/title/Title';
+import MainInput from './components/inputs/main-input/MainInput';
+import List from './components/list/List';
+import TotalPrice from './components/total-price/TotalPrice';
 
 function App() {
-  return <div className='app'></div>;
+  return (
+    <div className='app'>
+      <Switch textLabel='DM' />
+      <Title />
+      <MainInput />
+      <List />
+      <TotalPrice totalPriceValue='0' />
+    </div>
+  );
 }
 
 export default App;
