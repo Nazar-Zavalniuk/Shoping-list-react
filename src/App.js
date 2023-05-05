7b6d7de4a156cf640be84f80763312c0ae4c946b
Zavalniuk-Nazar <nazarzaval99@gmail.com>
2023-05-05 11:23:27 +0300

    Added "global BigInt" comment to avoid conflict with "eslint". The function of calculating the final price has been completely replaced.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* global BigInt */
 import React, {useEffect, useState} from 'react';
 import './style/App.css';
 import Switch from './components/switch/Switch';
@@ -23,23 +24,56 @@ function App() {
 
     items.forEach((item) => {
       if (item.marked.state && item.price !== '') {
-        prices.push(parseFloat(item.price));
+        prices.push(item.price);
       }
     });
 
-    function addFractions(fractions) {
-      // Сonvert fractions to whole numbers
-      const convertedFractions = fractions.map((f) => f * 100);
+    function calculateTotalCost(prices) {
+      const integerPart = [];
+      const fractionalPart = [];
 
-      // Add integers and convert back to fractions
-      const sum = convertedFractions.reduce((acc, val) => acc + val, 0) / 100;
+      prices.forEach((item) => {
+        const [integer = '', fraction = ''] = item.split('.');
+        integerPart.push(integer);
 
-      const roundedSum = sum.toFixed(2);
+        // We round the fraction in advance so that at the moment when the user marks the product as assembled
+        // and decides to edit the price, the already rounded fraction is included in the calculation
+        if (fraction.length >= 2) {
+          fractionalPart.push(fraction.slice(0, 2));
+        } else if (fraction.length === 1) {
+          fractionalPart.push(fraction + '0');
+        }
+      });
 
-      return roundedSum;
+      const sumInteger = integerPart.reduce((acc, val) => acc + BigInt(val), 0n);
+      const sumFractions = fractionalPart.reduce((acc, val) => acc + Number(val), 0);
+
+      // Converted the sum of fractions to separate the integer part from the fractional
+      const convertSumFractions = sumFractions / 100;
+
+      // From the converted sum of fractions, separated the integer part from the fractional
+      const [integer = '', fraction = ''] = String(convertSumFractions).split('.');
+
+      // I add the sum of integers obtained from the original number and the integer obtained
+      // as a result of adding the fractional parts of the original number,
+      // convert the addition result into a string and add the fractional part
+      // (depending on the sum of the fractional part, I round it up to hundredths by adding "0" to it).
+      // If the fractional part is missing, I append ".00" to the end
+      // so that the result displays the entire amount along with hundredths.
+      if (fraction.length === 2) {
+        return String(sumInteger + BigInt(integer)) + '.' + fraction;
+      } else if (fraction.length === 1) {
+        return String(sumInteger + BigInt(integer)) + '.' + fraction + '0';
+      } else {
+        return String(sumInteger + BigInt(integer)) + '.00';
+      }
     }
 
-    setTotalPrice(addFractions(prices));
+    if (prices.length === 0) {
+      setTotalPrice('0.00');
+    } else {
+      setTotalPrice(calculateTotalCost(prices));
+    }
   }, [items]);
 
   return (
